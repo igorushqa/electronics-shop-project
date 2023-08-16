21cd81b8b19fec9c64b88787c8ac589116ac622a
igorushqa <since1925@mail.ru>
2023-08-16 22:13:52 +0700

    HW4_final

--- src/phone.py
+++ src/phone.py
@@ -6,11 +6,23 @@ class Phone(Item):
     класс `Phone` содержит все атрибуты класса `Item`
     и дополнительно атрибут, содержащий количество поддерживаемых сим-карт
     """
+
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if number_of_sim > 0 and isinstance(number_of_sim, int):
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
 
 
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.phone import Phone
 
 
@@ -16,3 +18,7 @@ class TestPhone:
     def test_str_phone(self):
         phone1 = Phone("iPhone 14", 120_000, 5, 2)
         assert str(phone1) == 'iPhone 14'
+
+    def test_number_of_sim(self):
+        phone1 = Phone("iPhone 14", 120_000, 5, 0)
+        assert phone1.number_of_sim == 0
